7d1871309bec5a91f5fbce532396a3397aa6257a
Ilhom Utkirov <inspiration9595@gmail.com>
2023-04-11 15:14:44 +0200

    Initial commit

---
+++ myproject/owners/views.py
@@ -0,0 +1,31 @@
+# owners --> views.py
+from flask import Blueprint,render_template,redirect,url_for
+from myproject import db
+from myproject.models import Owner
+from myproject.owners.forms import AddForm
+
+owners_blueprint = Blueprint('owners',
+                              __name__,
+                              template_folder='templates/owners')
+
+
+#note /owners_add - it is done in registering the blueprint
+@owners_blueprint.route('/add', methods=['GET', 'POST'])
+def add():
+    form = AddForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        pup_id = form.pup_id.data
+        # Add new owner to database
+        new_owner = Owner(name, pup_id)
+        db.session.add(new_owner)
+        db.session.commit()
+
+        return redirect(url_for('puppies.list')) # diifference here not just 'list' but 'puppies.list'
+    return render_template('add_owner.html',form=form)
+
+
+
+
+
